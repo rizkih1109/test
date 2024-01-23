4676bda904b8b4a830365950851488b2bf182519
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-01-24 06:52:26 +0700

    search, pagination, and sort users done

--- ODMtest/routes/users.js
+++ ODMtest/routes/users.js
@@ -3,9 +3,27 @@ var router = express.Router();
 const User = require('../models/User')
 
 router.get('/', async (req, res, next) => {
+  const { name, phone, page = 1, sortBy = '_id', sortMode = 'asc' } = req.query
+  const params = {}
+  const sort = {}
+  sort[sortBy] = sortMode
+
+  if (name) {
+    params['name'] = new RegExp(name, 'i')
+  }
+
+  if (phone) {
+    params['phone'] = new RegExp(phone, 'i')
+  }
+
+  const limit = 3
+  const offset = (page - 1) * limit
+  const total = await User.countDocuments(params) 
+  const pages = Math.ceil(total / limit)
+
   try {
-    const users = await User.find()
-    res.json(users)
+    const users = await User.find(params).sort(sort).limit(limit).skip(offset)
+    res.json({data: users, page, pages})
   } catch (err) {
     res.status(500).json(err)
   }
